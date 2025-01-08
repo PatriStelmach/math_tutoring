0b786da414dd9add318101e41dce54cb8914ec3f
PatriStelmach <patrzy108@gmail.com>
2025-01-08 19:16:43 +0100

    title change

--- main/index.php
+++ main/index.php
@@ -9,7 +9,7 @@ include '../functions/db_connect.php'; // Dodajemy połączenie z bazą danych
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ja jestem madziunia seksi cipka spod Iławy</title>
+    <title>Korepetycje z matematyki</title>
     <link rel="stylesheet" href="/css/style.css">
 </head>
 
